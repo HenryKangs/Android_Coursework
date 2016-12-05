fd7ffcac27d130ece51d0145cb7dec01e51d8890
HenryKangs <gear7755@gmail.com>
2016-12-05 20:35:21 +0000

    Commit Successful! (Jeonghwa Kang)

---
+++ app/src/main/java/com/example/gear7_000/android_coursework/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.example.gear7_000.android_coursework;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    MediaPlayer mySong;
+
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_splash);
+        mySong = MediaPlayer.create(SplashActivity.this, R.raw.splashsound);
+        mySong.start();
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this,LoginActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
+
